1fd1a9dedf513bc7a137379adc8941a4651351cb
Sahana240107 <sahana240107@gmail.com>
2026-02-21 23:36:23 +0530

    deployed backend

--- ai_firewall_extension/content.js
+++ ai_firewall_extension/content.js
@@ -10,7 +10,7 @@ function debounce(fn, delay) {
 
 async function checkTextWithAPI(text) {
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://sahana-24-redactron-api.hf.space/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
